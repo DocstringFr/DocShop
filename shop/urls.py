840283184f58581a0faffc26759b4403999bdf60
Thibault <houdon.thibault@gmail.com>
2021-11-05 16:32:35 -0400

    Finish checkout process

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from store.views import index, product_detail, add_to_cart, cart, delete_cart, create_checkout_session
+from store.views import index, product_detail, add_to_cart, cart, delete_cart, create_checkout_session, stripe_webhook, checkout_success
 from accounts.views import signup, logout_user, login_user
 
 from shop import settings
@@ -14,7 +14,9 @@ urlpatterns = [
     path('login/', login_user, name="login"),
     path('logout/', logout_user, name="logout"),
     path('cart/', cart, name="cart"),
-    path('cart/create-checkout-session', create_checkout_session, name="create-checkout-session"),
+    path('stripe-webhook/', stripe_webhook, name="stripe-webhook"),
+    path('cart/create-checkout-session/', create_checkout_session, name="create-checkout-session"),
+    path('cart/success/', checkout_success, name="checkout-success"),
     path('cart/delete/', delete_cart, name="delete-cart"),
     path('product/<str:slug>/', product_detail, name="product"),
     path('product/<str:slug>/add-to-cart/', add_to_cart, name="add-to-cart"),
--- store/views.py
+++ store/views.py
@@ -1,7 +1,13 @@
+from datetime import datetime
+
 import stripe
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
+from accounts.models import Shopper
 from shop import settings
 from store.models import Product, Cart, Order
 
@@ -52,11 +58,15 @@ def create_checkout_session(request):
             line_items=line_items,
             mode='payment',
             locale='fr',
-            success_url="http://127.0.0.1:8000/",
-            cancel_url="http://127.0.0.1:8000/",
+            success_url=request.build_absolute_uri(reverse('checkout-success')),
+            cancel_url=request.build_absolute_uri(reverse('cart')),
     )
 
     return redirect(session.url)
+
+
+def checkout_success(request):
+    return render(request, 'store/success.html')
 
 
 def delete_cart(request):
@@ -65,3 +75,38 @@ def delete_cart(request):
         cart.delete()
 
     return redirect('index')
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    endpoint_secret = "whsec_PNuis6u3LGEXaoOmo6JC8T2Twompq1iR"
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+
+    try:
+        event = stripe.Webhook.construct_event(payload, sig_header, endpoint_secret)
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        return complete_order(session=session)
+
+    return HttpResponse(status=200)
+
+
+def complete_order(session):
+    try:
+        user_email = session["customer_details"]["email"]
+    except KeyError:
+        return HttpResponse("Invalid user email", status=404)
+
+    user = get_object_or_404(Shopper, email=user_email)
+    user.cart.ordered = True
+    user.cart.ordered_date = timezone.now()
+    user.cart.save()
+    return HttpResponse(status=200)
